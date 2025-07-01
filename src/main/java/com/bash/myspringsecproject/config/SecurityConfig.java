6af14aae68ad26108aef6c803416796d9dd6fa56
Bashir Muhammed <bashmufol@gmail.com>
2025-07-01 19:28:28 +0100

    updated

---
+++ src/main/java/com/bash/myspringsecproject/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.bash.myspringsecproject.config;
+
+public class SecurityConfig {
+}
---
+++ src/main/java/com/bash/myspringsecproject/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bash.myspringsecproject.repository;
+
+import com.bash.myspringsecproject.model.MyAppUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<MyAppUsers,Long> {
+    Optional<MyAppUsers> findByUsername(String username);
+}
