fd6283b5c684f61996ff961778b3e192e636ebff
Bashir Muhammed <bashmufol@gmail.com>
2025-07-01 21:45:30 +0100

    updated

--- src/main/java/com/bash/myspringsecproject/controller/UsersController.java
+++ src/main/java/com/bash/myspringsecproject/controller/UsersController.java
@@ -1,4 +1,24 @@
 package com.bash.myspringsecproject.controller;
 
+import com.bash.myspringsecproject.model.MyAppUsers;
+import com.bash.myspringsecproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UsersController {
+    private final UserRepository repo;
+    @GetMapping("/users")
+    public String getUsers(){
+        return "List of users is here";
+    }
+    @PostMapping("/signup")
+    public MyAppUsers signUp(@RequestBody MyAppUsers user){
+        r
+        return user;
+    }
 }
---
+++ src/main/java/com/bash/myspringsecproject/service/MyUserService.java
@@ -0,0 +1,4 @@
+package com.bash.myspringsecproject.service;
+
+public class MyUserService {
+}
