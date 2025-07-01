27012f3b559d2ae06fcce1e2c1716d79ffcd904e
Bashir Muhammed <bashmufol@gmail.com>
2025-07-01 20:12:36 +0100

    Updated

--- src/main/java/com/bash/myspringsecproject/config/SecurityConfig.java
+++ src/main/java/com/bash/myspringsecproject/config/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.bash.myspringsecproject.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/login", "signup").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 }
---
+++ src/main/java/com/bash/myspringsecproject/controller/UsersController.java
@@ -0,0 +1,4 @@
+package com.bash.myspringsecproject.controller;
+
+public class UsersController {
+}
