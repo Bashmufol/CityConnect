71235395d6c545bc23d1c7fa7295498ea98b8f1e
Bashir Muhammed <bashmufol@gmail.com>
2025-07-23 21:18:35 +0100

    updated

--- src/main/java/com/bash/myspringsecproject/service/MyUserDetailsService.java
+++ src/main/java/com/bash/myspringsecproject/service/MyUserDetailsService.java
@@ -18,7 +18,7 @@ public class MyUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String input) {
         Optional<MyAppUsers> userOptional = repo.findByUsername(input);
         if (userOptional.isEmpty()) {
-           userOptional = repo.findByEmail(input);htndndmeckndckjbjhdiewhdfiwuecommit -am "kkdsvbsd kldfks"
+           userOptional = repo.findByEmail(input);htndndmeckndckjbjhdiewhdfiwuecommit -am "kkdsvbsd kldfks"sfsjbczmnjz
         }
         if (userOptional.isPresent()) {
             MyAppUsers user = userOptional.get();
